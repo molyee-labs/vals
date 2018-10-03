ee1338c81fa2e78be7c4f56fc65326b472782519
Alex Sarapulov <1molyee@gmail.com>
2018-10-03 13:14:32 +0700

    [api] added profile and user mods

---
+++ api/src/user/account.rs
@@ -0,0 +1,10 @@
+use super::*;
+
+pub fn add(u:User) -> User {
+}
+
+pub fn delete(u:User) -> User {
+}
+
+pub fn find(id:Uid) -> Option<User> {
+}
---
+++ api/src/user/mod.rs
@@ -0,0 +1,10 @@
+type Uid = u64;
+
+struct User {
+    id: Uid,
+    name: String,
+    email: String,
+    password: String,
+    phone: String,
+    
+}
