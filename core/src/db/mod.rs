6543a4c42004e5438b417f8d798bcf946b9c85d9
Alex Sarapulov <1molyee@gmail.com>
2018-12-09 10:07:05 +0700

    [core] db trait downgrade

--- core/src/db/mod.rs
+++ core/src/db/mod.rs
@@ -1,50 +1,6 @@
-use result::Result;
+use ::result::Result;
 use std::convert::From;
 
-pub enum DbType {
-    SMALLINT(i16),
-    INT(i32),
-    BIGINT(i64),
-    TEXT(String),
-    REAL(f32),
-    DOUBLE(f64),
-}
-
-impl From<i16> for DbType {
-    fn from(v: i16) -> Self {
-        DbType::SMALLINT(v)
-    }
-}
-
-impl From<i32> for DbType {
-    fn from(v: i32) -> Self {
-        DbType::INT(v)
-    }
-}
-
-impl From<i64> for DbType {
-    fn from(v: i64) -> Self {
-        DbType::BIGINT(v)
-    }
-}
-
-impl From<f32> for DbType {
-    fn from(v: f32) -> Self {
-        DbType::REAL(v)
-    }
-}
-
-impl From<f64> for DbType {
-    fn from(v: f64) -> Self {
-        DbType::DOUBLE(v)
-    }
-}
-
-impl From<&str> for DbType {
-    fn from(v: &str) -> Self {
-        DbType::TEXT(v.to_string())
-    }
-}
 
 pub trait Db {
     fn func<T>(&self, func: &str, args: &[DbType]) -> Result<T>;
