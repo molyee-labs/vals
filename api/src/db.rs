7710df2b4dddaa19e7c3ea886645ae9509563b28
Alex Sarapulov <1molyee@gmail.com>
2018-12-09 09:56:43 +0700

    [api] update dbpostgres implementation

--- api/src/db.rs
+++ api/src/db.rs
@@ -1,13 +1,29 @@
 use core::db::Db;
-use postgres::{Connection, TlsMode};
+use core::result::*;
+use postgres::{stmt::Statement, types::*, Connection, TlsMode};
 
 pub struct DbPostgres {
     conn: Connection,
 }
 
-impl Db for DbPostgres {
-    fn new(dsn: &str, dbname: &str) -> Db {
+impl DbPostgres {
+    pub fn new(dsn: &str, dbname: &str) -> Self {
         let conn = Connection::connect(dsn, TlsMode::None)?;
         Db { conn }
+    }
+}
+
+impl Db for DbPostgres {
+    fn func<T>(&self, func: &str, args: &[DbType]) -> Result<T> {
+        let stmt = self.conn.prepare_cached("SELECT " + func + "($1,")?;
+        stmt.query()
+    }
+
+    fn proc<T>(&self, proc: &str, args: &[DbType]) -> Result<T> {
+        Err(Error::NotImplemented)
+    }
+
+    fn view<T>(&self, view: &str) -> Result<T> {
+        Err(Error::NotImplemented)
     }
 }
