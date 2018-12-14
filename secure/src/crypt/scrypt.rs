148d9907f83995ceb97525d14f89261ca3753cb4
Alex Sarapulov <1molyee@gmail.com>
2018-12-14 19:34:09 +0700

    [secure] optional scrypt hashing

---
+++ secure/src/crypt/scrypt.rs
@@ -0,0 +1,52 @@
+use crate::random;
+use scrypt::{scrypt, ScryptParams};
+use super::password::*;
+
+pub struct ScryptHasher {
+    params: ScryptParams
+}
+
+use crate::result::*;
+
+impl From<scrypt::errors::InvalidParams> for Error {
+    fn from(error: scrypt::errors::InvalidParams) -> Self {
+        Error::ScryptInvalidParams(error)
+    }
+}
+
+impl From<scrypt::errors::InvalidOutputLen> for Error {
+    fn from(error: scrypt::errors::InvalidOutputLen) -> Self {
+        Error::ScryptInvalidOutputLen(error)
+    }
+}
+
+impl ScryptHasher {
+    pub fn new() -> Result<Self> {
+        let params = ScryptParams::new(15, 8, 1)?;
+        Ok(Hasher { params })
+    }
+}
+
+impl Hasher for ScryptHasher {
+    fn get(&self, password: &[u8]) -> Result<Password> {
+        let mut salt = [0u8; 16];
+        random::next_bytes(&mut salt);
+        let mut hash = [0u8; 128];
+        scrypt(password, &salt, &self.params, &mut hash)?;
+        Ok(Password { hash, salt })
+    }
+
+    fn check(&self, password: &[u8], origin: &Password) -> Result<()> {
+        let mut hash = [0u8; 128];
+        scrypt(password, &origin.salt, &self.params, &mut hash)?;
+        if origin.hash[..] == hash[..] {
+            Ok(())
+        } else {
+            Err(Error::IncorrectPassword)
+        }
+    }
+}
+
+fn encode(password: &[u8]) -> Result<Password> {
+    Hasher::new()?.get(password)
+}
